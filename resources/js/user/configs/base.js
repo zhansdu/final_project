f24dfe04077f6df5929c3b582af161aba163d33e
zhan <zhanzhaksilik@gmail.com>
2021-05-17 03:33:23 +0600

    changed base url for heroku again

--- resources/js/user/configs/base.js
+++ resources/js/user/configs/base.js
@@ -1,5 +1,5 @@
 export default {
-    baseURL: 'https://sdulibrary.herokuapp.com/',
+    baseURL: 'https://sdulibrary.herokuapp.com',
     socketsBaseURL: 'http://localhost:3000',
     api: '/api/'
 }
